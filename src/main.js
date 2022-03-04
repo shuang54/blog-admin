aef5366e02b34d4fd437f184d1e3d04a24365195
shuang <2921056281@qq.com>
2022-03-04 17:10:56 +0800

    引入了vue-router,pinia,编辑了vite.config.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import router from './route'
+import { createPinia } from 'pinia'
+const app = createApp(App)
+app.use(router)
+app.use(createPinia())
+app.mount('#app')
---
+++ src/route/index.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HelloWorld from '@/components/HelloWorld.vue'
+const routerHistory = createWebHistory()
+const router = createRouter({
+  history: routerHistory,
+  routes: [
+    {
+      path: '/',
+      component: HelloWorld
+    },
+  ]
+})
+export default router
